2a53f10295d2346498be04f44d78fb5b12880d75
Lauren Shin <Lauren@MacBook-Pro-9.local>
2019-09-09 12:46:25 -0700

    rename project

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = u'Shin-Lauren'
+project = u'Motion Planning'
 copyright = u'2019, Lauren Shin'
 author = u'Lauren Shin'
 
